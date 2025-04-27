c1a23517ea88ef4c4399866115e2150af0e78126
Aniruddha Ravi <aniruddha.ravi1992@gmail.com>
2025-04-27 20:41:15 +0530

    fix bug

--- projectshelf/settings.py
+++ projectshelf/settings.py
@@ -34,7 +34,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
 ]
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
